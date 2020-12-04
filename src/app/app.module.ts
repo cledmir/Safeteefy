9dee80077eb7ec2eac15ec88e412d7ae093e2a5d
cledmir <56932765+cledmir@users.noreply.github.com>
2020-12-04 13:35:19 -0500

    ffff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { HttpClientModule } from '@angular/common/http';
 import { UrgenciesComponent } from './pages/urgencies/urgencies.component';
 import { GuardiansComponent } from './pages/guardians/guardians.component';
 import { UrgencyComponent } from './pages/urgency/urgency.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -24,7 +32,15 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatCardModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/guardians/guardians.component.ts
+++ src/app/pages/guardians/guardians.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
+import {Guardian} from '../../models/guardian';
+import {MatTableDataSource} from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+import {HttpDataService} from '../../services/http-data.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-guardians',
   templateUrl: './guardians.component.html',
   styleUrls: ['./guardians.component.css']
 })
-export class GuardiansComponent implements OnInit {
-
-  constructor() { }
+export class GuardiansComponent implements OnInit, AfterViewInit {
+  GuardianData: Guardian;
+  dataSource = new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'username', 'first name', 'last name', 'email', 'address', 'gender', 'actions'];
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  constructor(private httpDataService: HttpDataService, private router: Router) {
+    this.GuardianData = {} as Guardian;
+  }
 
   ngOnInit(): void {
+    this.dataSource.sort = this.sort;
+    this.getAllGuardians();
+    console.log(this.dataSource);
+  }
+  ngAfterViewInit(): void{
+    this.dataSource.paginator = this.paginator;
+  }
+  applyFilter(event: Event): void{
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+  getAllGuardians(): void{
+    this.httpDataService.getAllGuardians().subscribe((response: any) => {
+      this.dataSource = response;
+    });
   }
 
 }
